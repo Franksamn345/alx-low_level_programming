bb785c06600453f6de74b58312c6667be4c962a3
Franksamn345 <franklinesamntos@gmail.com>
2023-06-16 08:06:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to code
+ *
+ * Return: when the program returns 0 then program is successfull
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
