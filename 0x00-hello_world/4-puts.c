0cbb5f5f5b8bf5e9aab79b9f93b8a2e9f4ac58e4
Franksamn345 <franklinesamntos@gmail.com>
2023-06-16 07:52:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to code
+ *
+ * Return: when the program returns 0 its a success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
