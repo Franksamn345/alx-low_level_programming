52744ef8b02252642e419a099b48e984955b16f7
Franksamn345 <franklinesamntos@gmail.com>
2023-06-19 22:38:47 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, 58);
+	write(2, "\n", 1);
 	return (1);
 }
