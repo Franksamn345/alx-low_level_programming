8b6bcd202b50a273a8823eba28fd2f65212a2dac
Franksamn345 <franklinesamntos@gmail.com>
2023-06-20 13:25:00 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *  * main: Entry point into the code
- *   * Description: prints "_putchar" to the stdout
- *    * Return 0 (successfull)
- *     */
+ * main: Entry point into the code
+ * Description: prints "_putchar" to the stdout 
+ * Return 0 (successfull)
+ */
 int main(void)
 {
-	        _putchar('_');
-		        _putchar('p');
-			        _putchar('u');
-				        _putchar('t');
-					        _putchar('c');
-						        _putchar('h');
-							        _putchar('a');
-								        _putchar('r');
-									        _putchar('\n');
-										        return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
