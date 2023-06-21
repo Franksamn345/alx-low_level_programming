7af890311dc9d3bc071892f10ee7f622a1c7e582
Franksamn345 <franklinesamntos@gmail.com>
2023-06-20 17:12:10 -0700

    Is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _islower - Entry point into the code
+ * _isalpha - Entry point into the code
  * lowercase character. Another cases, shows
  * 0
  * Return: 0 (successfull)
  * @c: The character in ASCII code
  */
-int _islower(int c)
+int  _isalpha(int c)
 {
-	if (c >= 97 && c >= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
