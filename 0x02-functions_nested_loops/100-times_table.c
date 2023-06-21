da738448dbc0b43759a2a295ed8fbf348d9f041f
Franksamn345 <franklinesamntos@gmail.com>
2023-06-21 09:02:47 -0700

    0x02-functions_nested_loops/

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
- * print_times_table - prints times table for numbers from 0-14
+ * print_times_table - prints times table for numbers from 0-15
+ *
  * @n: An input integer value
+ *
  * Return: Nothing
+ *
  */
 void print_times_table(int n)
 {
