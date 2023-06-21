4dfd6d5ab3d0f55789a00afe1f4ff2c227c701ad
Franksamn345 <franklinesamntos@gmail.com>
2023-06-20 18:36:30 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit 
+ * Return: last digit
+ * @n: Input number as an integer
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
