16d4d35d2d2046e9482ab66c189e0e3c5284c1b3
Franksamn345 <franklinesamntos@gmail.com>
2023-06-21 02:15:41 -0700

    Print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
  * print_alphabet_x10- Entry point into the code
+ *
  * Description: prints "_putchar" to the stdout
+ *
  * Return 0 (successfull)
+ *
  */
 void print_alphabet_x10(void)
 {
 	int i;
 	int l;
-	
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (l = 97; l <= 122; l++)
