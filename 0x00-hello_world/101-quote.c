6cbea477d61f7e09e214dc7621e5709db4c4fd4e
Franksamn345 <franklinesamntos@gmail.com>
2023-06-21 08:24:12 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Main function - Prints a line to the standard error.
- * @return 1 indicating successful execution.
+ * main- Entry to the code
+ *Return:1
+ *
  */
 int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(2, msg, 58);
 	write(2, "\n", 1);
 	return (1);
