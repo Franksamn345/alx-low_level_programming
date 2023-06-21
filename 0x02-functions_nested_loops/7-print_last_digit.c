d8fd4e6059b70f45746fabe4583626b1365bd640
Franksamn345 <franklinesamntos@gmail.com>
2023-06-20 19:23:20 -0700

    print to 98

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * print_last_digit - Prints the last digit 
- * Return: last digit
- * @n: Input number as an integer
+ * print_last_digit - Check numbers
+ * @n: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: last digit of number l 
  */
 int print_last_digit(int n)
 {
