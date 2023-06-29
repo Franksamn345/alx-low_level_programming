7c1526dfa3495f8eaa9bb6f525c66769dce13288
Franksamn345 <franklinesamntos@gmail.com>
2023-06-29 03:32:25 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,15 @@
 
 /**
  * _strncat - a function that concatenates two strings.
+ *
  * @dest: an input string
+ *
  * @src: an input string
+ *
  * @n: an input integer
+ *
  * Return: A pointer to the resulting string
+ *
  */
 char *_strncat(char *dest, char *src, int n)
 {
