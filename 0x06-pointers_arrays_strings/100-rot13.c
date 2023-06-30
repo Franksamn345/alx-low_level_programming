ecf01513c383f5a3e7bd93d0fb1dcfdf2472b297
Franksamn345 <franklinesamntos@gmail.com>
2023-06-30 14:47:34 -0700

    rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,26 +2,30 @@
 
 /**
  * rot13 -  a function that encodes a string using rot13.
+ *
  * @s: An input string to encode using rot13
+ *
  * Return: An encode string
+ *
  */
 char *rot13(char *s)
 {
-	int i = 0;
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
+
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		for (j = 0; j < 52; j++)
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
 		}
-		i++;
 	}
 	return (s);
 }
