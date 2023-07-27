3feffbdc7e2c20383c388fa09bbc2763df46d474
Franksamn345 <franklinesamntos@gmail.com>
2023-07-27 15:11:24 -0700

    Linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
+void free_list(list_t *head)
+{
+
+if (head == NULL)
+
+return;
+
+if (head->next != NULL)
+
+free_list(head->next);
+
+free(head->str);
+
+free(head);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# 0x12. C - Singly linked lists
+## Learning objectives
+### General
+* When and why using linked lists vs arrays
+* How to build and use linked lists
